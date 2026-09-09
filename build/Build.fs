5e70f1f6d92c2e84dbdb04f34591a0d07ebfb531
David Hrabě <david.hrabe@almamedia.com>
2026-09-09 14:38:52 +0200

    Refactor build infrastructure to Alma.Build

--- build/Build.fs
+++ build/Build.fs
@@ -1,16 +1,8 @@
-// ========================================================================================================
-// === F# / Project fake build ==================================================================== 1.6.0 =
-// --------------------------------------------------------------------------------------------------------
-// Options:
-//  - no-clean   - disables clean of dirs in the first step (required on CI)
-//  - no-lint    - lint will be executed, but the result is not validated
-// ========================================================================================================
-
+open Alma.Build
 open Fake.Core
 open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 
-open ProjectBuild
 open Utils
 
 [<EntryPoint>]
